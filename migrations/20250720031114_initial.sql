953efbff2c914879a2c521f75cb7e7739bd12616
MichaelScofield111 <141600921+MichaelScofield111@users.noreply.github.com>
2025-07-20 17:38:58 +0800

    feature: support db migration and user model

--- migrations/20250720031114_initial.sql
+++ migrations/20250720031114_initial.sql
@@ -1,13 +1,15 @@
 -- this file is used for postgresql database initialization
 -- create user table
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    id bigserial PRIMARY KEY,
     fullname VARCHAR(64) NOT NULL,
-    -- hashed argon2 password
-    password VARCHAR(264) NOT NULL,
+    -- hashed argon2 password length 97
+    password_hash VARCHAR(97) NOT NULL,
     email VARCHAR(64) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP
 );
+-- create unique index for users for email
+CREATE UNIQUE INDEX IF NOT EXISTS email_idx ON users(email);
 
 -- create chat type: sigle, group, private_channel, public_channel
 CREATE TYPE chat_type AS ENUM ('single', 'group', 'private_channel', 'public_channel');
@@ -19,20 +21,17 @@ CREATE TABLE IF NOT EXISTS chats (
     type chat_type NOT NULL,
     -- user id list
     members BIGINT[] NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP
 );
 
 -- crate message table
 CREATE TABLE IF NOT EXISTS messages (
     id BIGSERIAL PRIMARY KEY,
-    chat_id BIGINT NOT NULL,
-    sender_id BIGNUT NOT NULL,
+    chat_id BIGINT NOT NULL REFERENCES chats(id),
+    sender_id BIGINT NOT NULL REFERENCES users(id),
     content TEXT NOT NULL,
     images TEXT[],
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (chat_id) REFERENCES chats(id),
-    FOREIGN KEY (sender_id) REFERENCES users(id),
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP
 );
 
 
@@ -40,4 +39,4 @@ CREATE TABLE IF NOT EXISTS messages (
 CREATE INDEX IF NOT EXISTS chat_id_created_at_idx ON messages(chat_id, created_at DESC);
 
 -- create index for messages for sender_id
-CRATE INDEX IF NOT EXISTS sender_id_idx ON messages(sender_id);
+CREATE INDEX IF NOT EXISTS sender_id_idx ON messages(sender_id, created_at DESC);
